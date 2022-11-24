60e2346ef717599f89f28f6dcb19af07142da3a1
Thiago Santos <thiago_593@hotmail.com>
2022-11-23 22:16:52 -0300

    EX011

---
+++ exercicios/aula11/ex011.js
@@ -0,0 +1,6 @@
+let vel= 10
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel>60){ //condição simples
+    console.log('Velocidade permitida ultrapassada')
+}
+console.log("diriga usando cinto de segurança")
---
+++ exercicios/aula11/ex012.js
@@ -0,0 +1,7 @@
+var pais ='china'
+console.log(`Vivendo em ${pais}`)
+if(pais== 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro')
+}
