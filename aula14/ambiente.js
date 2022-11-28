ca05990de11668ea72cdcd8fbff3896976afdc01
Thiago Santos <thiago_593@hotmail.com>
2022-11-27 21:42:44 -0300

    .

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,10 +1,13 @@
-var c=1
+for(let i=1;i<=10;i++){
+    console.log (`Teste ${i}`)
+}
+/*var c=1
 do{
     console.log(`Teste ${c}`)
     c++
 }while(c <= 6)
 
-/*let c = 1
+let c = 1
 while (c <= 10) {
     console.log(`Teste ${c}`)
     c++
