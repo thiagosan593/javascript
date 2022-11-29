07e15ae7e29e034001f817704026c112b0f59c25
Thiago Santos <thiago_593@hotmail.com>
2022-11-29 15:27:00 -0300

    Funções

---
+++ exercicios/aula17/fatorial.js
@@ -0,0 +1,12 @@
+function fatorial(n){
+    let fat = 1
+    for(let c=n;c>1;c--){
+        console.log(`FAT ${fat} C= ${c}`)
+      
+       fat *= c
+
+    }
+    return fat
+}
+
+console.log(fatorial(5))
---
+++ exercicios/aula17/soma.js
@@ -0,0 +1,6 @@
+function soma(n1,n2){
+    return n1 + n2
+}
+
+let res= soma(1,210)
+console.log(res)
